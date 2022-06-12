2c71a366f3d739398bd1be9e8827afccd465d8ef
depaul171 <57862676+depaul171@users.noreply.github.com>
2022-06-12 17:03:38 +0300

    Update forms.py

--- employee_registry/forms.py
+++ employee_registry/forms.py
@@ -11,7 +11,7 @@ class EmployeeForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(EmployeeForm, self).__init__(*args, **kwargs)
-        self.fields['position'].empty_label='Select position'
+        self.fields['position'].empty_label='Select position'  #this sets the doted line dropdown option to 'Select position' as its label name 
         self.fields['email'].required = False      #sets the Email field filling to optional in the form
 
         
